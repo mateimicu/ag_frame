c40a46225cf35c563f32da1ea506e47097c4584c
Micu Matei-Marius <mmicu@cloudbasesolutions.com>
2016-12-03 01:42:39 +0200

    Add a base item for out framework

---
+++ ag_frame/base_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+Base item for every in this library.
+"""
+
+
+# pragma pylint: disable=too-few-public-methods
+class BaseItem(object):
+    """Base class for every item."""
+    _args = []
+
+    @classmethod
+    def set_args(cls, args):
+        """Set the arguments."""
+        cls._args = args
