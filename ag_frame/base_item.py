f6eeb16dc84f6db104f955b5d0f45a517e704b0c
Micu Matei-Marius <mmicu@cloudbasesolutions.com>
2016-12-03 17:43:07 +0200

    Fix pylint warning

--- ag_frame/base_item.py
+++ ag_frame/base_item.py
@@ -7,7 +7,7 @@ Base item for every in this library.
 # pragma pylint: disable=too-few-public-methods
 class BaseItem(object):
     """Base class for every item."""
-    _args = []
+    _args = None
 
     @classmethod
     def set_args(cls, args):
