1b0e99a26cc23731ce01f9d862399232de0edb61
Micu Matei-Marius <mmicu@cloudbasesolutions.com>
2016-12-04 11:35:25 +0200

    Fix typo

--- ag_frame/command_line.py
+++ ag_frame/command_line.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from ag_frame.algorithms import factory as a_facotry
+from ag_frame.algorithms import factory as a_factory
 from ag_frame.functions import factory as f_factory
 from ag_frame.runners import run_alg
 
@@ -19,9 +19,9 @@ def main():
     """The main function for this mini-framework."""
     parser = prepare_parser()
     subparsers = parser.add_subparsers(help="Choose a runner.")
-    algorithms = a_facotry.algorithms_factory()
+    algorithms = a_factory.algorithms_factory()
     functions = f_factory.functions_factory()
 
-    base_runner = run_alg.RunAlgorithm(parser, subparsers, 
-            algorithms, functions)
+    base_runner = run_alg.RunAlgorithm(parser, subparsers,
+                                       algorithms, functions)
     base_runner.evaluate()
