963cd03c7d88cce03b5d887ba9c0473b4f99071e
Micu Matei-Marius <mmicu@cloudbasesolutions.com>
2016-12-25 00:10:47 +0200

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='ag_frame',
-      version='0.1',
+      version='0.2',
       description='Genetic Algorithms',
       author='Micu Matei-Marius',
       author_email='micumatei@gmail.com',
       license='MIT',
-      packages=['ag_frame'],
+      packages=find_packages(),
       entry_points = {
         'console_scripts': ['ag_frame=ag_frame.command_line:main'],
       },
