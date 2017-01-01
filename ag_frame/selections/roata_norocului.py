fc6562fc66c9602a7d61a418831d4b9effab8ade
Micu Matei-Marius <mmicu@cloudbasesolutions.com>
2017-01-01 22:37:35 +0200

    Fix selection wheel

--- ag_frame/selections/roata_norocului.py
+++ ag_frame/selections/roata_norocului.py
@@ -31,22 +31,30 @@ class Wheel(base.BaseSelection):
                         item in population]
         total_fit = sum(fit_per_item)
 
-        probability_per_item = [float(total_fit) / item_fit for
+        probability_per_item = [item_fit / float(total_fit)  for
                                 item_fit in fit_per_item]
 
         item_chances = [0]
         for index, item in enumerate(population):
             item_chances.append(item_chances[-1] + probability_per_item[index])
 
+        # eliminate the first item the 0
+        item_chances.pop(0)
+
         random_item = random.random()
         item = None
         # select the item j with
         # item_chances[j] < random_item <= item_chances[j+1]
-        for index in range(len(item_chances)-2):
-            if item_chances[index] < random_item <= item_chances[index+1]:
-                item = population[index-1]
+        for index, value in enumerate(item_chances):
+            if random_item <= value:
+                item = population[index]
+                break
+        else:
+            # To few people in the population
+            item = population.pop()
 
         # remove selected item
         population.remove(item)
+
 
         return item, population
